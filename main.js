a8c5d3dddfbf526111009ff04fae5098f43efba1
dungbubu <nguyenmanhdung1996@gmail.com>
2023-10-24 14:52:38 +0700

    Add spinner animation

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
+import * as THREE from "three";
 
-import * as THREE from 'three';
-
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader }    from 'three/addons/loaders/GLTFLoader.js';
-import { RGBELoader }    from 'three/addons/loaders/RGBELoader.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
 let camera, scene, renderer;
 
@@ -11,60 +10,75 @@ init();
 render();
 
 function init() {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
 
-  const container = document.createElement( 'div' );
-  document.body.appendChild( container );
-
-  camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set( 0, 90, 0 );
+  camera = new THREE.PerspectiveCamera(
+    20,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 90, 0);
 
   scene = new THREE.Scene();
 
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.toneMapping = THREE.ACESFilmicToneMapping;
   renderer.toneMappingExposure = 1;
-  container.appendChild( renderer.domElement );
+  container.appendChild(renderer.domElement);
 
   new RGBELoader()
     // environment
-    .setPath( './libJS/threejs/examples/textures/equirectangular/' )
-    .load( 'royal_esplanade_1k.hdr', function ( texture ) {
+    .setPath("./libJS/threejs/examples/textures/equirectangular/")
+    .load("royal_esplanade_1k.hdr", function (texture) {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.background = texture;
       scene.environment = texture;
       render();
 
       // model
-      const loader = new GLTFLoader().setPath( './element3D/' );
-      loader.load( 'mibs.glb', function ( gltf ) {
-        gltf.scene.scale.set(.05*gltf.scene.scale.x, .05*gltf.scene.scale.y, .05 * gltf.scene.scale.z);
-        scene.add( gltf.scene );
-        render();
-      } );
-    } );
+      const loader = new GLTFLoader().setPath("./element3D/");
+      loader.load("mibs.glb", function (gltf) {
+        gltf.scene.scale.set(
+          0.05 * gltf.scene.scale.x,
+          0.05 * gltf.scene.scale.y,
+          0.05 * gltf.scene.scale.z
+        );
 
-  const controls = new OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', render ); // use if there is no animation loop
+        function animate() {
+          requestAnimationFrame(animate);
+          // gltf.scene.rotation.x += 0.01;
+          // gltf.scene.rotation.y += 0.01;
+          gltf.scene.rotation.z += 0.01;
+          render();
+        }
+        
+        scene.add(gltf.scene);
+        animate();
+        render();
+      });
+    });
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render); // use if there is no animation loop
   controls.minDistance = 2;
   controls.maxDistance = 10;
-  controls.target.set( 0, 0, - 0.2 );
+  controls.target.set(0, 0, -0.2);
   controls.update();
 
-  window.addEventListener( 'resize', onWindowResize );
-
+  window.addEventListener("resize", onWindowResize);
 }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
 
-//
-
 function render() {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
